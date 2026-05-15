f0c067f0abb97a1f74d17be8891aa0e32c913033
Leslie Guillen <lguille6@stu.southtexascollege.edu>
2026-05-14 23:39:10 -0500

    added globals

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+import js from "@eslint/js";
+import globals from "globals";
+import pluginVue from "eslint-plugin-vue";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default [
+  {
+    name: "app/files-to-lint",
+    files: ["**/*.{js,mjs,jsx,vue}"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+    },
+  },
+
+  {
+    name: "app/files-to-ignore",
+    ignores: [
+      "**/dist/**",
+      "**/dist-ssr/**",
+      "**/coverage/**",
+      "**/node_modules/**",
+    ],
+  },
+
+  js.configs.recommended,
+  ...pluginVue.configs["flat/recommended"],
+  eslintConfigPrettier,
+];
